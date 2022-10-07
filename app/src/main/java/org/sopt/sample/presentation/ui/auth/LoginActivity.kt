ec554580dba004b01e58a8660415ab1937d7e1b1
sujin-kk <sjoo0002@gmail.com>
2022-10-07 16:26:34 +0900

    [CHORE] add activity to manifest

--- app/src/main/java/org/sopt/sample/presentation/ui/auth/LoginActivity.kt
+++ app/src/main/java/org/sopt/sample/presentation/ui/auth/LoginActivity.kt
@@ -1,6 +1,5 @@
 package org.sopt.sample.presentation.ui.auth
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import org.sopt.sample.R
 import org.sopt.sample.databinding.ActivityLoginBinding
