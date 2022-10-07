072e2bd882d290a44fc5cb631c4eb1f5c404dae1
sujin-kk <sjoo0002@gmail.com>
2022-10-07 21:20:34 +0900

    [FEAT] introduce activity view

---
+++ app/src/main/java/org/sopt/sample/presentation/ui/home/introduce/IntroduceViewModel.kt
@@ -0,0 +1,9 @@
+package org.sopt.sample.presentation.ui.home.introduce
+
+import androidx.lifecycle.ViewModel
+
+class IntroduceViewModel: ViewModel() {
+    var userId: String = ""
+    var userPw: String = ""
+    var userMbti: String = ""
+}
